68c1ecf118ff84510b1e1374ede79b78e7c83e0d
praveenlingamsetti <praveenlingamsetti333@gmail.com>
2024-06-10 16:05:35 +0530

    Fix: desktop background and header is fixed

--- src/components/hamburger-searchbar/hamburger-searchbar.js
+++ src/components/hamburger-searchbar/hamburger-searchbar.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import "../../styles/hamburger-and-searchbar/hamburger-and-searchbar.scss";
 import { IoSearch } from "react-icons/io5";
 import BuckleBeatsIcon from "../../assets/Bucklebeats Icon.svg";
-import Cookies from 'js-cookie'
-import toast  from "react-hot-toast";
+import Cookies from "js-cookie";
+import toast from "react-hot-toast";
 import QrCode from "../qr-code/qr-code";
+import { backdropClasses } from "@mui/material";
 
 const Hamburger = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -58,21 +59,23 @@ const Hamburger = () => {
   };
   return (
     <div className="main-qrcode-container">
-      <header className="mobile-header">
-        <div className="hamburger" onClick={toggleMenu}>
-          <div className="burger"></div>
-          <div className="burger"></div>
-          <div className="burger"></div>
-        </div>
-        <div className="search-container">
-          <div className="search-input-wrapper">
-            <input type="text" />
-            <img src={BuckleBeatsIcon} alt="heart" className="search-bg-icon" />
-            <IoSearch className="search-icon" />
+      <div className="header-container">
+        <header className="mobile-header">
+          <div className="hamburger" onClick={toggleMenu}>
+            <div className="burger"></div>
+            <div className="burger"></div>
+            <div className="burger"></div>
           </div>
-        </div>
-      </header>
-     {menuOpen && (
+          <div className="search-container">
+            <div className="search-input-wrapper">
+              <input type="text" />
+              <img src={BuckleBeatsIcon} alt="heart" className="search-bg-icon" />
+              <IoSearch className="search-icon" />
+            </div>
+          </div>
+        </header>
+      </div>
+      {menuOpen && (
         <div className="menu">
           <p className="menu-item" onClick={() => handleMenuItemClick("qr-codes-screen")}>
             Menu Item 1
@@ -88,9 +91,7 @@ const Hamburger = () => {
           </p>
         </div>
       )}
-      <div className="qrcodes-containers">
-        {renderSelectedComponent()}
-      </div>
+      <div className="qrcodes-containers">{renderSelectedComponent()}</div>
     </div>
   );
 };
