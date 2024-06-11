76aa2c38312824eb2b2e52dbaa606b8c870bf6e1
chowdeswarigit <chowdeswari599@gmail.com>
2024-06-11 12:09:26 +0530

    Fix:Forgot password alignment issue fixed

--- src/components/qr-code/qr-code.js
+++ src/components/qr-code/qr-code.js
@@ -5,25 +5,84 @@ import axios from 'axios';
 import EnableQRCode from '../enable-qrcode/enable-qrcode';
 import toast from 'react-hot-toast';
 
+// const ItemCard = ({ item }) => {
+//   const [qrcode, setQRcode] = useState(null);
+//   const [activeId, setActiveId] = useState(null);
+
+//   const handleOpen = (id) => {
+//     if (activeId !== id) {
+//       setQRcode(id);
+//       setActiveId(id);
+//     } else {
+//       setQRcode(null);
+//       setActiveId(null);
+//     }
+//   };
+
+//   const handleClose = () => {
+//     setQRcode(null);
+//   };
+//   const handleTurnOn = async (qr_planet_id) => {
+//     const token = Cookies.get("accessToken");
+//     const url = process.env.REACT_APP_PRODUCTION_URL;
+
+//     try {
+//       const response = await axios.put(
+//         `${url}/api/qrcode/change-status`,
+//         { code: qr_planet_id },
+//         { headers: { Authorization: `Bearer ${token}` } }
+//       )
+//       toast.success(response.data.resultMessage.en, { duration: 2000 });
+//       handleClose()
+//     } catch (error) {
+//       console.error('Error making API call:', error);
+//       toast.error(error.response.data.resultMessage.en, { duration: 5000 });
+//     }
+//   };
+//   return (
+//     <div className="item-card">
+//       <img src={item?.image_url} alt={item.name} className="item-image" />
+//       <h5 style={{ fontSize: '12px', color: '#1B3E51', marginTop: "6px", fontWeight: '640' }}>{item.name}</h5>
+//       <div className='switch-container'>
+//         <span className="lost-mode-text">{item.is_lost ? 'Lost Mode' : 'Lost Mode'}</span>
+//         <div className="toggle-container" onClick={() => handleOpen(item?.qr_planet_id)}>
+//           {/* <div className={`toggle-button ${item.is_lost ? 'active' : ''}`} ></div> */}
+//           <div className={`toggle-button ${item.is_lost===true ? 'active' : ''}`}></div>
+//           {/* <div className={`toggle-button ${activeId === item._id  ? 'active' : ''}`}></div> */}
+
+//         </div>
+//       </div>
+//         <EnableQRCode openModal={qrcode === item.qr_planet_id} closeModal={() => handleTurnOn(item.qr_planet_id)} id={item._id} />
+      
+//       {/* <EnableQRCode openModal={qrcode === item.id} closeModal={() => handleTurnOn(item.id, item.qr_planet_id)} id={item.id} qr_planet_id={item.qr_planet_id} /> */}
+//       {/* <EnableQRCode openModal={qrcode === item.qr_planet_id} closeModal={() => handleTurnOn(item.id,item?.qr_planet_id)} id={item.id} /> */}
+//     </div>
+//   );
+// };
 const ItemCard = ({ item }) => {
   const [qrcode, setQRcode] = useState(null);
   const [activeId, setActiveId] = useState(null);
 
   const handleOpen = (id) => {
-    if (activeId !== id) {
-      setQRcode(id);
-      setActiveId(id);
+    if (item.is_lost) {
+      setActiveId((prevId) => (prevId === id ? null : id));
     } else {
-      setQRcode(null);
-      setActiveId(null);
+      if (activeId !== id) {
+        setQRcode(id);
+        setActiveId(id);
+      } else {
+        setQRcode(null);
+        setActiveId(null);
+      }
     }
   };
 
   const handleClose = () => {
     setQRcode(null);
   };
-  const handleTurnOn = async (id, qr_planet_id) => {
-    const token = Cookies.get("accessToken");
+
+  const handleTurnOn = async (qr_planet_id) => {
+    const token = Cookies.get('accessToken');
     const url = process.env.REACT_APP_PRODUCTION_URL;
 
     try {
@@ -31,34 +90,34 @@ const ItemCard = ({ item }) => {
         `${url}/api/qrcode/change-status`,
         { code: qr_planet_id },
         { headers: { Authorization: `Bearer ${token}` } }
-      )
+      );
       toast.success(response.data.resultMessage.en, { duration: 2000 });
-      handleClose()
+      handleClose();
     } catch (error) {
       console.error('Error making API call:', error);
       toast.error(error.response.data.resultMessage.en, { duration: 5000 });
     }
   };
-  console.log(item.qr_planet_id)
+
   return (
     <div className="item-card">
       <img src={item?.image_url} alt={item.name} className="item-image" />
-      <h5 style={{ fontSize: '12px', color: '#1B3E51', marginTop: "6px", fontWeight: '640' }}>{item.name}</h5>
-      <div className='switch-container'>
-        <span className="lost-mode-text">{item.is_lost ? 'Lost Mode' : 'Lost Mode'}</span>
+      <h5 style={{ fontSize: '12px', color: '#1B3E51', marginTop: '6px', fontWeight: '640' }}>{item.name}</h5>
+      <div className="switch-container">
+        <span className="lost-mode-text">{item.is_lost ? 'Lost Mode' : 'Normal Mode'}</span>
         <div className="toggle-container" onClick={() => handleOpen(item?.qr_planet_id)}>
-          {/* <div className={`toggle-button ${item.is_lost ? 'active' : ''}`} ></div> */}
-          <div className={`toggle-button ${item.is_lost===true ? 'active' : ''}`}></div>
-
+          <div className={`toggle-button ${activeId === item.qr_planet_id || item.is_lost ? 'active' : ''}`}></div>
         </div>
       </div>
-        <EnableQRCode openModal={qrcode === item.qr_planet_id} closeModal={() => handleTurnOn(item.id, item.qr_planet_id)} id={item.id} />
-      
-      {/* <EnableQRCode openModal={qrcode === item.id} closeModal={() => handleTurnOn(item.id, item.qr_planet_id)} id={item.id} qr_planet_id={item.qr_planet_id} /> */}
-      {/* <EnableQRCode openModal={qrcode === item.qr_planet_id} closeModal={() => handleTurnOn(item.id,item?.qr_planet_id)} id={item.id} /> */}
+      <EnableQRCode
+        openModal={qrcode === item.qr_planet_id}
+        closeModal={() => handleTurnOn(item.qr_planet_id)}
+        id={item._id}
+      />
     </div>
   );
 };
+
 
 const QrCode = ({ searchInput }) => {
   const [qrCodes, setQrCodes] = useState([]);
@@ -92,41 +151,43 @@ const QrCode = ({ searchInput }) => {
           }
         }
       );
-      if (pageNum === 1) {
-        setQrCodes(response.data.qrCodes);
-      } else {
-        setQrCodes(prevQrCodes => [...prevQrCodes, ...response.data.qrCodes]);
-      }
-      setLoading(false);
+      setQrCodes(response.data.qrCodes);
+
+
+      // if (pageNum === 1) {
+      // } else {
+      //   setQrCodes(prevQrCodes => [...prevQrCodes, ...response.data.qrCodes]);
+      // }
+      // setLoading(false);
     } catch (error) {
       console.error("Error fetching QR codes:", error);
       setLoading(false);
     }
   }, [limit]);
 
-  // Initial fetch with name=''
-  useEffect(() => {
-    fetchQrCodes(1, '');
-  }, []);
+  // useEffect(() => {
+  //   fetchQrCodes(page, '');
+  // }, []);
 
   useEffect(() => {
-    if (debouncedSearchInput.length >= 3 || debouncedSearchInput.length === 0) {
+    if (debouncedSearchInput.length >= 3 ) {
       setPage(1); // Reset the page state when the search input changes
-      fetchQrCodes(1, debouncedSearchInput);
+      fetchQrCodes(page, debouncedSearchInput);
     }
-  }, [debouncedSearchInput, fetchQrCodes]);
+  }, [debouncedSearchInput, fetchQrCodes,page]);
 
   
 
-  const handleScroll = () => {
-    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || loading) return;
-    setPage(prevPage => prevPage + 1);
-  };
+  // const handleScroll = () => {
+  //   if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || loading) return;
+  //   setPage(prevPage => prevPage + 1);
+  //   fetchQrCodes(page,debouncedSearchInput)
+  // };
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [loading]);
+  // useEffect(() => {
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => window.removeEventListener('scroll', handleScroll);
+  // }, [loading]);
 
   useEffect(() => {
     if ((debouncedSearchInput.length >= 3 || debouncedSearchInput.length === 0) && page > 1) {
@@ -137,16 +198,18 @@ const QrCode = ({ searchInput }) => {
   useEffect(() => {
     if (debouncedSearchInput.length < 3) {
       setPage(1);
-      fetchQrCodes(1, '');
+      fetchQrCodes(page, '');
     }
-  }, [debouncedSearchInput, fetchQrCodes]);
+  }, [debouncedSearchInput, fetchQrCodes,page]);
 
   return (
     <div className="app">
-      <div className="item-list">
-        {qrCodes.map(item => (
-          <ItemCard key={item.id} item={item}  />
-        ))}
+       <div className="item-list">
+        {qrCodes.length === 0  ? (
+          <div className="no-data-container"><h1>No data found</h1></div>
+        ) : (
+          qrCodes.map((item) => <ItemCard key={item._id} item={item} />)
+        )}
       </div>
       {loading && <div>Loading more items...</div>}
       <div className="footer-buttons">
--- src/components/register/register-form.js
+++ src/components/register/register-form.js
@@ -7,25 +7,26 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import cat from "../../assets/Cat.gif";
 import bag from "../../assets/bagimage.gif";
-import dog from '../../assets/dog.gif'
+import dog from "../../assets/dog.gif";
 import cycle from "../../assets/Bicycle LT_1.gif";
 import toast from "react-hot-toast";
-import { FaCheckCircle} from "react-icons/fa";
+import { FaCheckCircle } from "react-icons/fa";
 
 const RegisterForm = () => {
-  
-const [currentImageIndex, setCurrentImageIndex] = useState(0);
-const [isRegistred,setIsRegistred]  = useState(false)
-const [passwordVisible, setPasswordVisible] = useState(false);
-const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
-const {
-  register,
-  handleSubmit,
-  watch,
-  reset,
-  formState: { errors },
-} = useForm();
-const images = [bag, cycle, cat, dog];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isRegistred, setIsRegistred] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useForm();
+  const images = [bag, cycle, cat, dog];
 
   useEffect(() => {
     const imageInterval = setInterval(() => {
@@ -63,10 +64,10 @@ const images = [bag, cycle, cat, dog];
         return "";
     }
   };
+
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[.!@$%^&*])[^\s]{8,}$/;
 
- 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
@@ -74,6 +75,20 @@ const images = [bag, cycle, cat, dog];
   const toggleConfirmPasswordVisibility = () => {
     setConfirmPasswordVisible(!confirmPasswordVisible);
   };
+
+  const password = watch("password");
+  const confirmPassword = watch("confirmPassword");
+
+  useEffect(() => {
+    if (confirmPassword && confirmPassword !== password) {
+      setError("confirmPassword", {
+        type: "validate",
+        message: "Passwords do not match",
+      });
+    } else {
+      clearErrors("confirmPassword");
+    }
+  }, [confirmPassword, password, setError, clearErrors]);
 
   const onformSubmit = async (data) => {
     const url = process.env.REACT_APP_PRODUCTION_URL;
@@ -86,89 +101,103 @@ const images = [bag, cycle, cat, dog];
         platform: "Android",
       });
       toast.success(response?.data?.resultMessage?.en, {
-        duration: 5000, 
-      });      reset();
-      setIsRegistred(true)
+        duration: 5000,
+      });
+      reset();
+      setIsRegistred(true);
     } catch (error) {
-      toast.error(error.response.data.resultMessage.en,{ duration: 5000 });
+      toast.error(error.response.data.resultMessage.en, { duration: 5000 });
     }
   };
 
   return (
     <div className="regestation-container">
-      {isRegistred ? <div className="form-container">
-        {/* <FaCheckCircle className="verification-icon" /> */}
-        <FaCheckCircle className="verification-message" ></FaCheckCircle>
-         <h6 style={{color:"#E4E9F1"}}>Please check your email and complete the verification process to log in.</h6> </div> : <div className="form-container">
-        <div className={`${getBackgroundClassName()}`}></div>
-        <h1>Welcome!</h1>
-        <form onSubmit={handleSubmit(onformSubmit)}>
-          <div className="form-group">
-            <input className="input-box" type="text" id="name" {...register("name", { required: "Name is required" })} placeholder="Name" />
-            {errors.name && <span className="error">{errors.name.message}</span>}
-          </div>
-          <div className="form-group">
-            <input
-              className="input-box"
-              type="email"
-              id="email"
-              {...register("email", {
-                required: "Email is required",
-                pattern: {
-                  value: emailRegex,
-                  message: "Invalid email address",
-                },
-              })}
-              placeholder="Email"
-            />
-            {errors.email && <span className="error">{errors.email.message}</span>}
-          </div>
-          <div className="password-container form-group">
-            <input
-              className="input-box"
-              type={passwordVisible ? "text" : "password"}
-              id="password"
-              {...register("password", {
-                required: "Password is required",
-                pattern: {
-                  value: passwordRegex,
-                  message: "Password must be at least 8 characters contains a digit, an uppercase letter, and a special character.",
-                },
-              })}
-              placeholder="Password"
-            />
-            <button type="button" className="show-password" onClick={togglePasswordVisibility}>
-              {passwordVisible ?<VisibilityOffTwoToneIcon/> : <VisibilityTwoToneIcon />}
+      {isRegistred ? (
+        <div className="form-container">
+          <FaCheckCircle className="verification-message"></FaCheckCircle>
+          <h6 style={{ color: "#E4E9F1" }}>
+            Please check your email and complete the verification process to log in.
+          </h6>
+        </div>
+      ) : (
+        <div className="form-container">
+          <div className={`${getBackgroundClassName()}`}></div>
+          <h1>Welcome!</h1>
+          <form onSubmit={handleSubmit(onformSubmit)}>
+            <div className="form-group">
+              <input
+                className="input-box"
+                type="text"
+                id="name"
+                {...register("name", { required: "Name is required" })}
+                placeholder="Name"
+              />
+              {errors.name && <span className="error">{errors.name.message}</span>}
+            </div>
+            <div className="form-group">
+              <input
+                className="input-box"
+                type="email"
+                id="email"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: emailRegex,
+                    message: "Invalid email address",
+                  },
+                })}
+                placeholder="Email"
+              />
+              {errors.email && <span className="error">{errors.email.message}</span>}
+            </div>
+            <div className="password-container form-group">
+              <input
+                className="input-box"
+                type={passwordVisible ? "text" : "password"}
+                id="password"
+                {...register("password", {
+                  required: "Password is required",
+                  pattern: {
+                    value: passwordRegex,
+                    message:
+                      "Password must be at least 8 characters contains a digit, an uppercase letter, and a special character.",
+                  },
+                })}
+                placeholder="Password"
+              />
+              <button type="button" className="show-password" onClick={togglePasswordVisibility}>
+                {passwordVisible ? <VisibilityOffTwoToneIcon /> : <VisibilityTwoToneIcon />}
+              </button>
+              {errors.password && <span className="error">{errors.password.message}</span>}
+            </div>
+            <div className={`password-container form-group ${errors.password ? "with-error" : ""}`}>
+              <input
+                className="input-box"
+                type={confirmPasswordVisible ? "text" : "password"}
+                id="confirmPassword"
+                {...register("confirmPassword", {
+                  required: "Confirm Password is required",
+                  validate: (value) => value === watch("password") || "Passwords do not match",
+                })}
+                placeholder="Confirm Password"
+              />
+              <button type="button" className="show-password" onClick={toggleConfirmPasswordVisibility}>
+                {confirmPasswordVisible ? <VisibilityOffTwoToneIcon /> : <VisibilityTwoToneIcon />}
+              </button>
+              {errors.confirmPassword && <span className="error">{errors.confirmPassword.message}</span>}
+            </div>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <img alt="dog" className={`${getClassName()}`} src={images[currentImageIndex]}></img>
+            </div>
+            <button type="submit" className="register-button">
+              SignUp
             </button>
-            {errors.password && <span className="error">{errors.password.message}</span>}
-          </div>
-          <div className={`password-container form-group ${errors.password ? "with-error" : ""}`}>
-            <input
-              className="input-box"
-              type={confirmPasswordVisible ? "text" : "password"}
-              id="confirmPassword"
-              {...register("confirmPassword", {
-                required: "Confirm Password is required",
-                validate: (value) => value === watch("password") || "Passwords do not match",
-              })}
-              placeholder="Confirm Password"
-            />
-            <button type="button" className="show-password" onClick={toggleConfirmPasswordVisibility}>
-              {confirmPasswordVisible ? <VisibilityOffTwoToneIcon/> : <VisibilityTwoToneIcon />}
-            </button>
-            {errors.confirmPassword && <span className="error">{errors.confirmPassword.message}</span>}
-          </div>
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <img alt="dog" className={`${getClassName()}`} src={images[currentImageIndex]}></img>
-          </div>
-          <button type="submit" className="register-button">
-            SignUp
-          </button>
-          <p className="Login">     
-            <Link to="/Login">Already have an accout? Login</Link>
-          </p>
-        </form>
-      </div> }
+            <p className="Login">
+              <Link to="/Login">Already have an accout? Login</Link>
+            </p>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
