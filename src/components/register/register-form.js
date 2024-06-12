cd56556a6d56c247ad05831b1948b0ee0ebd4644
chowdeswarigit <chowdeswari599@gmail.com>
2024-06-12 12:07:26 +0530

    Fix:Spacing between the input fields fixed

--- src/components/register/register-form.js
+++ src/components/register/register-form.js
@@ -170,7 +170,7 @@ const RegisterForm = () => {
               </button>
               {errors.password && <span className="error">{errors.password.message}</span>}
             </div>
-            <div className={`password-container form-group ${errors.password ? "with-error" : ""}`}>
+            <div className={`password-container form-group ${errors?.password?.message && errors?.password?.message !== "Password is required" ? "with-error" : ""}`}>
               <input
                 className="input-box"
                 type={confirmPasswordVisible ? "text" : "password"}
