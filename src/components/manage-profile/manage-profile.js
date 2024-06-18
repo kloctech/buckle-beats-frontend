443e961325018e50931f89765cf4209730e0c3ee
divyadarshini <divyadarshini.d@kloctechnologies.com>
2024-06-18 12:47:50 +0530

    updated profile

--- src/components/manage-profile/manage-profile.js
+++ src/components/manage-profile/manage-profile.js
@@ -25,44 +25,40 @@ const ManageProfile = () => {
             }
         } catch (error) { }
     };
-
+ const profiles = [
+    {
+        Name: "Alex"
+    },
+    {
+        Name: "Silvi"
+    },
+    {
+        Name: "Dixon"
+    },
+    {
+        Name: "Harry"
+    },
+    {
+        Name: "Alicia"
+    }
+ ]
     return (
        
-            <div className="login-container">
-                 <div className="manage-profile">
+            <div className="login-container">      
+                <div className="manage-profile">
                 <h1>Switch Profile</h1>
+                <Link to="/" className="close-menu">X</Link>
                 <ul className="manage-profile-list">
-                  <li>
-                    <div className="manage-profile-bg">
-                    <span>A</span>
-                    </div>
-                    <h4>Alex</h4>
-                  </li>
-                  <li>
-                   <div className="manage-profile-bg">
-                    <span>S</span>
-                    </div>
-                    <h4>Silvi</h4>
-                  </li>
-                  <li>
-                   <div className="manage-profile-bg">
-                    <span>D</span>
-                    </div>
-                    <h4>Dixon</h4>
-                  </li>
-                  <li>
-                  <div className="manage-profile-bg">
-                    <span>H</span>
-                    </div>
-                    <h4>Harry</h4>
-                  </li>
-                  <li>
-                  <div className="manage-profile-bg">
-                    <span>A</span>
-                    </div>
-                    <h4>Alicia</h4>
-                  </li>
-                  
+                {profiles.slice(0, 5).map((item) =>{  
+                    return(
+                        <li key={item.Name}>
+                        <div className="manage-profile-bg">
+                        <span>{item.Name.charAt(0)}</span>
+                        </div>
+                        <h4>{item.Name}</h4>
+                      </li>
+                    )    
+                  })}
                   <li class="invite">
                   <div className="manage-profile-bg">
                     <span>Invite</span>
