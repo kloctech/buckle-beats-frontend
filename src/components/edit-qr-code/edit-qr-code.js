87ff798e04d4c547734343123e7237531f959d5d
chowdeswarigit <chowdeswari599@gmail.com>
2024-07-03 17:55:23 +0530

    Fix: fixes for mobile screen

--- src/components/edit-qr-code/edit-qr-code.js
+++ src/components/edit-qr-code/edit-qr-code.js
@@ -243,7 +243,7 @@ const EditQRCode = () => {
               }}
               disabled={!isEdited}
             >
-              Edit
+             Update
             </button>
           </div>
         </form>
