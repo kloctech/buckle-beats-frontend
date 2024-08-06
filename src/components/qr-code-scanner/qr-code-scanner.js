609899b8ec53830a12514c306dc808eca6816766
chowdeswarigit <chowdeswari599@gmail.com>
2024-08-06 19:18:01 +0530

    Fix: Lost mode page email and phone number given link,camera not access error message,edit and add qr-code name validation

--- src/components/qr-code-scanner/qr-code-scanner.js
+++ src/components/qr-code-scanner/qr-code-scanner.js
@@ -1,150 +1,4 @@
-// import React, { useState, useRef, useEffect, useCallback } from "react";
-// import { BrowserMultiFormatReader, NotFoundException } from "@zxing/library";
-// import "../../styles/qr-code-scanner/qr-code-scanner.scss";
-// import { useNavigate, Link } from "react-router-dom";
-// import api from "../../middleware/api";
-// import icon from "../../assets/problem.gif";
-// import InvalidQrCode from "../invalid-qr-code/invalid-qr-code";
 
-// function QrCodeScanner() {
-//   const [code, setCode] = useState("");
-//   const [error, setError] = useState(null);
-//   const [userData, setUserData] = useState(null);
-//   const [showFormAnimation, setShowFormAnimation] = useState(false);
-
-//   const videoRef = useRef(null);
-//   const codeReaderRef = useRef(new BrowserMultiFormatReader());
-//   const navigate = useNavigate();
-
-//   const verifyQrCode = useCallback(async (code) => {
-//     try {
-//       const response = await api.get(`${process.env.REACT_APP_PRODUCTION_URL}/api/qrcode/details/${code}`, {});
-//       const qrData = response?.data?.qrCode;
-//       setUserData(qrData);
-//       if (qrData && !qrData.user_id) {
-//       } else {
-//       }
-//     } catch (err) {
-      
-//     }
-//   }, []);
-
-//   const startDecoding = useCallback(async () => {
-//     try {
-//       await codeReaderRef.current.decodeFromVideoDevice(undefined, videoRef.current, (result, err) => {
-//         if (result) {
-//           const code = result.getText().split("/").pop();
-//           setCode(code);
-//           setError("");
-//           codeReaderRef.current.reset();
-
-//           if (code.length === 6 && result.getText().includes("https://bucklebeats.qrplanet.com")) {
-//             verifyQrCode(code);
-//           } else {
-//             setShowFormAnimation(true);
-//           }
-//         }
-//         if (err && !(err instanceof NotFoundException)) {
-//           setError("Error accessing camera or scanning QR code.");
-//         }
-//       });
-//     } catch (error) {
-//       setError("Error initializing camera.");
-//     }
-//   }, [verifyQrCode]);
-
-//   useEffect(() => {
-//     startDecoding();
-//     const codeReader = codeReaderRef.current;
-//     return () => {
-//       if (codeReader) {
-//         codeReader.reset();
-//       }
-//     };
-//   }, [startDecoding]);
-
-//   const resetScanner = () => {
-//     setCode("");
-//     setError("");
-//     setUserData(null);
-//     setShowFormAnimation(false);
-//     if (codeReaderRef.current) {
-//       codeReaderRef.current.reset();
-//     }
-//     setTimeout(() => {
-//       startDecoding();
-//     }, 500);
-//   };
-
-//   const  heading =  userData?.user_id ? "This QR code is Already Registered" :"Something has gone wrong, don't worry. Let's try again.";
-
-//   const handleNextClick = () => {
-//     if (code) {
-//       navigate(`/add-qr-code/${code}`);
-//     }
-//   };
-//   return (
-//     <>
-//       {showFormAnimation  || userData?.user_id ? (
-//         <InvalidQrCode
-//           icon={icon}
-//           heading={heading}
-//           showQrCodeIcon={true}
-//           userId = {userData?.user_id}
-//           onClose={resetScanner} // Pass resetScanner as the onClose handler
-//         />
-//       ) : (
-//         <div className="qr-scanner form-container">
-//           {!code && (
-//             <div className="header-title">
-//               <h1>Activate QR</h1>
-//               <Link to="/" className="close-menu">
-//                 X
-//               </Link>
-//             </div>
-//           )}
-//           {!code ? (
-//             <div>
-//               <div className="video-container">
-//                 <video ref={videoRef} autoPlay />
-//               </div>
-//               <div style={{ marginTop: "20px" }}>
-//                 <p style={{ fontWeight: "200", color: "#fff" }}>Scan Each QR via your phone individually to activate.</p>
-//               </div>
-//             </div>
-//           ) : (
-//             <>
-//               {userData ? (
-//                 userData?.user_id ? (
-//                   ""
-//                 ) : (
-//                   <div className="qr-bg">
-//                   <img alt="qr-code" src={userData?.image_url}  />
-//                   </div>
-//                 )
-//               ) : (
-//                 <p style={{ color: "red" }}>{error}</p>
-//               )}
-//                {!userData?.user_id ? (
-//                 <div className="button-row">
-//                   <button onClick={() => navigate("/")} className="cta-button cancel-btn">
-//                     Cancel
-//                   </button>
-//                   <button onClick={handleNextClick} className="cta-button next-btn">
-//                     Next
-//                   </button>
-//                 </div>
-//               ) : null}
-
-//             </>
-//           )}
-//         </div>
-//       )}
-//     </>
-//   );
-// }
-
-// export default QrCodeScanner;
 import React, { useState, useRef, useEffect, useCallback } from "react";
 import { BrowserMultiFormatReader, NotFoundException } from "@zxing/library";
 import "../../styles/qr-code-scanner/qr-code-scanner.scss";
@@ -205,14 +59,16 @@ function QrCodeScanner() {
   }, [verifyQrCode]);
 
   useEffect(() => {
-    startDecoding();
+    if (!cameraAccessDenied) {
+      startDecoding();
+    }
     const codeReader = codeReaderRef.current;
     return () => {
       if (codeReader) {
         codeReader.reset();
       }
     };
-  }, [startDecoding]);
+  }, [cameraAccessDenied, startDecoding]);
 
   const resetScanner = () => {
     setCode("");
@@ -260,19 +116,20 @@ function QrCodeScanner() {
           )}
           {!code ? (
             <div>
-              <div className="video-container">
-                {cameraAccessDenied ? (
-                  <p style={{ color: "red", textAlign:"center" }}>Please allow camera access to scan QR codes.</p>
-                ) : (
-                  <video ref={videoRef} autoPlay />
-                )}
-              </div>
-              {cameraAccessDenied}
-              <div style={{ marginTop: "20px" }}>
-                <p style={{ fontWeight: "200", color: "#fff" }}>
-                  Scan Each QR via your phone individually to activate.
-                </p>
-              </div>
+              {!cameraAccessDenied ? ( 
+                <div className="video-container">
+              <video ref={videoRef} autoPlay />
+                </div>
+              ) : <p style={{ color: 'rgb(250, 111, 104)' }}>
+                    Please allow camera access to scan QR codes.
+                  </p> }
+              {!cameraAccessDenied && (
+                <div style={{ marginTop: "20px" }}>
+                  <p style={{ fontWeight: "200", color: "#fff" }}>
+                    Scan Each QR via your phone individually to activate.
+                  </p>
+                </div>
+              )}
             </div>
           ) : (
             <>
