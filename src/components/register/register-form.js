d2bd8cfe10cd9e5f46f92d77c191f7af830e22f8
praveenlingamsetti <praveenlingamsetti333@gmail.com>
2025-11-05 19:38:52 +0530

    Update: Email validation in user registration

--- src/components/register/register-form.js
+++ src/components/register/register-form.js
@@ -82,7 +82,7 @@ const RegisterForm = () => {
     }
   };
 
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])[^\s]{8,32}$/;
 
   const togglePasswordVisibility = () => {
