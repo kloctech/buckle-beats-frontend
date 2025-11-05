812758790021b6e2c2f9b68279bc8b9932eadc8f
praveenlingamsetti <praveenlingamsetti333@gmail.com>
2025-11-05 20:41:41 +0530

    Update: Email validation in user login

--- src/components/login/login-form.js
+++ src/components/login/login-form.js
@@ -115,7 +115,7 @@ const LoginPage = () => {
               {...register("email", {
                 required: "Email is required",
                 pattern: {
-                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/,
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
                   message: "Invalid email address",
                 },
               })}
